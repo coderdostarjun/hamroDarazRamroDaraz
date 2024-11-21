bf055bdec8458a30545b903fae3ea2f70b609c1b
coderdostarjun <khanalarjun225@gmail.com>
2024-11-21 21:10:09 +0545

    this is a file changed

---
+++ src/test/java/com/hamroDaraz/daraz/controller/Daraz.java
@@ -0,0 +1,10 @@
+package com.hamroDaraz.daraz.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+public class Daraz {
+    @RestController
+    @RequestBody
+
+}
---
+++ src/test/java/com/hamroDaraz/daraz/controller/shop.java
@@ -0,0 +1,13 @@
+package com.hamroDaraz.daraz.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+
+@Setter
+@Getter
+
+public class shop {
+
+}
