6cc6b233ffab911dc8a31c6a69debea164d88476
coderdostarjun <khanalarjun225@gmail.com>
2024-11-21 22:01:23 +0545

    hogaya

---
+++ src/test/java/com/hamroDaraz/daraz/controller/user.java
@@ -0,0 +1,11 @@
+package com.hamroDaraz.daraz.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class user {
+}
