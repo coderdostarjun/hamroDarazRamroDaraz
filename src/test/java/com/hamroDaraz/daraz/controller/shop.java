23bd34fdf8304c39c4d037c080156d0f25873355
coderdostarjun <khanalarjun225@gmail.com>
2024-11-21 22:49:35 +0545

    final commmit of today

--- src/test/java/com/hamroDaraz/daraz/controller/shop.java
+++ src/test/java/com/hamroDaraz/daraz/controller/shop.java
@@ -1,10 +1,11 @@
 package com.hamroDaraz.daraz.controller;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Service;
 
-
+@NoArgsConstructor
 @Setter
 @Getter
 
