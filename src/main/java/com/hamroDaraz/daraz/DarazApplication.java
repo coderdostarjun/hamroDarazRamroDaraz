40be559b0c89ac1775b3515983c6fb38cf0b60c9
coderdostarjun <khanalarjun225@gmail.com>
2024-11-21 20:43:51 +0545

    first commit

---
+++ src/main/java/com/hamroDaraz/daraz/DarazApplication.java
@@ -0,0 +1,13 @@
+package com.hamroDaraz.daraz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarazApplication.class, args);
+	}
+
+}
